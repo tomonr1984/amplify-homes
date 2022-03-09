9482fe0c00b8c9c97ae7325d5baec9531a6eec6e
萩野友紀 <hagino.tomonori@kyodo-d.jp>
2022-03-09 12:23:42 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { CardBCollection, NavBar, MarketingFooter } from './ui-components'
 
 function App() {
   return (
     <div className="App">
+      <NavBar width={"100vw"} />
+      <CardBCollection isPaginated itemsPerPage={3}/>
+      <MarketingFooter width={"100vw"} />
     </div>
   );
 }
